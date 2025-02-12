e164e5a53a1719032ce401010009eb7ec9abde30
VitalySLTfr <89817215938@yandex.ru>
2025-02-12 04:24:52 +0300

    Create 7.7.4.js

---
+++ 7.7.4.js
@@ -0,0 +1,7 @@
+const person = {};
+setFullName = function setFullName (fullName) {
+    this.fullName = fullName;
+}
+const setPersonFullName = setFullName.bind(person);
+setPersonFullName("John Smith");
+console.log(person.fullName);
